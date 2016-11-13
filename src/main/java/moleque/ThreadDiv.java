b3bf5abe0c1822bc191cb5421ae763b7bc3e5ed8
Nikos Mouchtaris <nikos.mouchtaris@redtulipsystems.com>
2016-11-13 15:54:19 +0000

    Pretty molque

---
+++ src/main/java/moleque/ThreadDiv.java
@@ -0,0 +1,18 @@
+package moleque
+
+public class ThreadDiv extends Thread{
+    private int num1, num2;
+
+    public ThreadDiv(int n1, int n2)
+    {
+        if (n2 == 0)
+            throw new IllegalArgumentException("Argument 'divisor' is 0");
+        num1 = n1;
+        num2 = n2;
+    }
+
+    public void run()
+    {
+         System.out.println("Div = " + (num1 / num2));
+    }
+}
