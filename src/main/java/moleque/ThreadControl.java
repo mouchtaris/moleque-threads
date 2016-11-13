9b1256d0bbd3f2fb50ad8610c6daf92d01531937
Nikos Mouchtaris <nikos.mouchtaris@redtulipsystems.com>
2016-11-13 16:05:35 +0000

    Add semaphores

--- src/main/java/moleque/ThreadControl.java
+++ src/main/java/moleque/ThreadControl.java
@@ -1,12 +1,18 @@
 package moleque;
 
+import java.util.concurrent.Semaphore;
+
 public class ThreadControl
 {
     public static void main(String[] args)
     {
-        ThreadAdd thr1 = new ThreadAdd(2,3);
-        ThreadSub thr2 = new ThreadSub(2,3);
-        ThreadMult thr3 = new ThreadMult(2,2,1);
+        Semaphore sem1 = new Semaphore(1),
+                  sem2 = new Semaphore(1),
+                  sem3 = new Sempahore(1);
+        ThreadAdd thr1 = new ThreadAdd(2,3, sem1);
+        ThreadSub thr2 = new ThreadSub(2,3, sem1, sem2);
+        ThreadMult thr3 = new ThreadMult(2,2,1, sem2, sem3);
+        ThreadDiv thr4 = new ThreadDiv(6, 2, sem3);
 
         thr1.start();
         thr2.start();
